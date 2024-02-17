6e7c8417f43fc0c12332463626caab6fa6f0f6f2
gitdeverstein <hei.andy.8@gmail.com>
2024-02-17 19:01:39 +0300

    chore: modification of the project

---
+++ doc-scanner/src/App.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import ConnexionPage from './components/ConnexionPage';
+
+function App() {
+  return (
+    <div>
+      <ConnexionPage/>
+    </div>
+  );
+}
+
+export default App;
