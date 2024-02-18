a27c50a13b218a2ccbfe3ef8d6982bdb23cd38ab
gitdeverstein <hei.andy.8@gmail.com>
2024-02-18 10:42:12 +0300

    chore: modification of App.js

--- doc-scanner/src/App.js
+++ doc-scanner/src/App.js
@@ -1,11 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './components/HomePage';
 import ConnexionPage from './components/ConnexionPage';
 
 function App() {
   return (
-    <div>
-      <ConnexionPage/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/homepage" element={<HomePage />} />
+        <Route path="/" element={<ConnexionPage />} />
+      </Routes>
+    </Router>
   );
 }
 
